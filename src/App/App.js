4db26026519da4cdc18e8b1a720d519edda3e88d
Mike <maykinaykii@gmail.com>
2018-10-22 11:19:40 +0400

    fetch data from vimeo public api

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,33 @@ import React, { Component } from "react";
 import "./app.scss";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.vimeoAccessToken = "02317c4726a1bcbeb27c55cb25bf6f86";
+        this.state = {
+            data: []
+        };
+    }
+
+    fetchFromVimeo = (query) => {
+        if (query) {
+            this.timeout && window.clearTimeout(this.timeout);
+            this.timeout = window.setTimeout(() => {
+                fetch(`https://api.vimeo.com/videos?access_token=${this.vimeoAccessToken}&query=${query}`)
+                .then((response) => {
+                    return response.json()
+                })
+                .then(response => {
+                    this.setState({
+                        data: response.data
+                    })
+                })
+                .catch(function(e) {
+                    console.log(e);
+                })
+            }, 450);
+        }
+    };
 
     render() {
         return (
